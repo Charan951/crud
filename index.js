860ef9358baee80b25771966d133fc802f9d2b0f
Charan951 <charanteja@speshway.com>
2026-02-04 11:21:51 +0530

    .env

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+dotenv.config();
+
+
+
+
+const name=process.env.name;
+console.log(`Name from env file is ${name}`);
+const port= process.env.port;
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+}   
+)
+
